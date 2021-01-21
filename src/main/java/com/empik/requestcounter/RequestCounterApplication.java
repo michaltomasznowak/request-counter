93593d7346d14b659ef8784a1665339b147b6b8b
michal.nowak <michal.nowak@openjawtech.com>
2021-01-21 08:46:35 +0000

    TASK-0 Initialize project

---
+++ src/main/java/com/empik/requestcounter/RequestCounterApplication.java
@@ -0,0 +1,13 @@
+package com.empik.requestcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empik/requestcounter/RequestCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empik.requestcounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
